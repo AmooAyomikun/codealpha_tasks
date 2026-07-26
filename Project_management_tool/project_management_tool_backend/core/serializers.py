30d1bdd1d740fadc8232123c5e6f805b9b6bb548
amooayomikun <amooquadri555@gmail.com>
2026-07-26 01:00:39 +0100

    Implementing Backend

--- Project_management_tool/project_management_tool_backend/core/serializers.py
+++ Project_management_tool/project_management_tool_backend/core/serializers.py
@@ -7,8 +7,8 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'avatar_url', 'created_at')
-        read_only_fields = ('id', 'created_at')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'avatar_url', 'date_joined')
+        read_only_fields = ('id', 'date_joined')
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
